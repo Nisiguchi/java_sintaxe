a901c2af4b2da5742573592d1216371eaef0dc24
Nicole <nlinonicole@gmail.com>
2024-05-29 19:29:19 -0300

    operadores mais um

---
+++ tipo-variavel/src/OperadoresDois.java
@@ -0,0 +1,25 @@
+public class OperadoresDois {
+
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        //String resultado = "";
+        //if(a==b)
+           // resultado = "verdadeiro";
+        //else
+            //resultado = "falso";
+
+        //System.out.println(resultado);
+
+        //String resultado = a==b ? "verdadeiro" : "falso";
+
+        int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+
+    }
+
+}
